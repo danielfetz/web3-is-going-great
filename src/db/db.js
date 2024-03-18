2f0dc654a0e75a22b7f1646df17633a29364c01e
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-03-18 10:57:55 +0100

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -5,13 +5,13 @@ import "firebase/auth";
 
 // Initialize Firebase
 const app = initializeApp({
-  apiKey: "AIzaSyDLAj91oJ3U_24VU2RW9511RKqYuf-sviQ",
-  authDomain: "web3-334501.firebaseapp.com",
+  apiKey: "AIzaSyAfvZUgXFZ9u00JYG4kIlu_7UJiog-pvC8",
+  authDomain: "test-01-c678f.firebaseapp.com",
   databaseURL: "https://web3-334501-default-rtdb.firebaseio.com",
-  projectId: "web3-334501",
-  storageBucket: "primary-web3-regional",
-  messagingSenderId: "645239520683",
-  appId: "1:645239520683:web:3e90522f06ea69822ce6ee",
+  projectId: "test-01-c678f",
+  storageBucket: "test-01-c678f.appspot.com",
+  messagingSenderId: "940910304070",
+  appId: "1:940910304070:web:267ebe1a6a9330f6322347"
 });
 
 export const db = getFirestore(app);
